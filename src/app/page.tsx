6bd9f63fa3c3f25cfe95b56a17981994a8d7450d
ahoang2502 <athoang252@gmail.com>
2024-02-14 10:38:37 +1100

    authentication sign-up form

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -32,7 +32,7 @@ export default function Home() {
 				<div className="py-20 mx-auto text-center flex flex-col items-center max-w-3xl">
 					<h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">
 						Your market place for high-quality{" "}
-						<span className="text-blue-600">digital assets</span>.
+						<span className="text-[#2f89f8]">digital assets</span>.
 					</h1>
 
 					<p className="mt-6 text-lg max-w-prose text-muted-foreground">
@@ -52,7 +52,7 @@ export default function Home() {
 				{/* TODO: List products */}
 			</MaxWidthWrapper>
 
-{/* Perks */}
+			{/* Perks */}
 			<section className="border-t border-gray-200 bg-gray-50">
 				<MaxWidthWrapper className="py-20">
 					<div className="grid grid-cols-1 gap-y-12 sm:grid-cols-2 sm:gap-x-6 lg:grid-cols-3 lg:gap-x-8 lg:gap-y-0">
